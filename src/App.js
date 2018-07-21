68c4b491983a2cc4105ac946e0fbbb4b80124938
Manuel Kruisz <kruisz88@gmail.com>
2018-07-21 05:12:50 +0200

    add nav to all sites

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     BrowserRouter as Router,
     Route
 } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar';
 import Welcome from './views/welcome/Welcome';
 import Login from './views/login/Login';
 import Overview from './views/overview/Overview';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <div>
-            <Route exact path="/" component={Welcome}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/overview" component={Overview}/>
-          </div>
-        </Router>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <div class="main">
+                        <div class="nav">
+                            <Navbar className="nav"/>
+                        </div>
+                        <div className="content">
+                        <Route exact path="/" component={Welcome}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/overview" component={Overview}/>
+                        </div>
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/views/overview/Overview.js
+++ src/views/overview/Overview.js
@@ -5,7 +5,6 @@ import './Overview.css';
 
 const Overview = () => (
     <div className="overview">
-        <Navigation />
         Overview
     </div>
 );
